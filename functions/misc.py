Minor fixes to basis, sppedup of jacobian

functions/misc.py
@@ -45,8 +45,7 @@ def knot_fn(ts, n, dataset):
         # construct knot locations
         for gapn, k0, k1, x0, x1, c in zip(kgn, keep[:-1], keep[1:], times[:-1], times[1:], copies[1:]):
             step = 2*(x1-x0)/(2*gapn+k0+k1)
-            cands = np.arange(x0-(1-k0)*step/2, x1+(3-k1)*step/2+step, step)
+            cands = np.arange(x0-(1-k0)*step/2, x1+(3-k1)*step/2, step)
             frag = cands[1:(gapn+1+k1)]
             knots.extend(frag)
         return sorted(knots)
-

modeller.py
@@ -52,7 +52,7 @@ class Model():
                    [ca.diag(ca.jacobian(self.basis[:, i], self.ts)) 
                     for i in range(self.K)]
                ).reshape((self.n, self.K))
-        self.basis_jacobian = ca.Function('bjac', [self.ts], [bjac])(self.observation_times)
+        self.basis_jacobian = np.array(ca.Function('bjac', [self.ts], [bjac])(self.observation_times))
 
         # create the objects that define the smooth, model parameters
         self.cs = [ca.MX.sym("c_"+str(i), self.K, 1) for i in range(self.s)]