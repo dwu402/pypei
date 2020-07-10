827822efc004b159376996dd7a79e6962b99a0ff
David Wu <dwu402@aucklanduni.ac.nz>
2020-07-10 16:24:44 +1200

    Chunked variance strucutre

--- examples/quadratic_functional.py
+++ examples/quadratic_functional.py
@@ -39,7 +39,11 @@ if visual:
 # symbolic representing the regressors
 ps = ca.SX.sym('p', 2)
 # symbolic representing the standard deviation
-std_dev = ca.SX.sym('s')
+# here, we represent the std devs over the entire time window
+# into separate chunks to estimate (though we know them to be the same)
+std_dev_primitives = ca.SX.sym('s', 3) # 3 chunks
+std_dev = ca.repmat(std_dev_primitives, 1, 10).T.reshape((-1,1))
+# print(std_dev)
 
 objective = pypei.objective.Objective()
 objective_config = {
@@ -59,7 +63,7 @@ objective.make(objective_config)
 solver = pypei.fitter.Solver()
 solver_config =  {
     # decision variables
-    'x': ca.vcat([ps, std_dev]),
+    'x': ca.vcat([ps, std_dev_primitives]),
     # functional form of the -ve log likelihood
     'f': objective.objective_function,
     # constraints
